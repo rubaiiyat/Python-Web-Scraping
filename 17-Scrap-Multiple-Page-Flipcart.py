dd7f53c7eb7fb0fe209cc138978735936cafb049
rubaiiyat <abirr1516@gmail.com>
2024-09-06 00:41:34 +0600

    Flipcut multiple page scrap

--- 17-Scrap-Multiple-Page-Flipcart.py
+++ 17-Scrap-Multiple-Page-Flipcart.py
@@ -1,41 +1,66 @@
-import requests
 import cloudscraper
 from bs4 import BeautifulSoup
 import pandas as pd
 
-url = "https://www.flipkart.com/search?q=acoustic+guitar&as=on&as-show=on&otracker=AS_Query_HistoryAutoSuggest_1_9_na_na_na&otracker1=AS_Query_HistoryAutoSuggest_1_9_na_na_na&as-pos=1&as-type=HISTORY&suggestionId=acoustic+guitar&requestId=979dca32-ba12-41f5-8d46-7514f22ada35&as-backfill=on"
-scraper = cloudscraper.create_scraper()
-res = scraper.get(url)
-print(res)
-
-
-soup = BeautifulSoup(res.text, "lxml")
-
 nameList = []
 priceList = []
-TotalRev = []
 reviewList = []
+TotalRev = []
 
-name = soup.find_all("a", class_="wjcEIp")
+for i in range(0, 16):
+    scraper = cloudscraper.create_scraper()
+    url = (
+        "https://www.flipkart.com/search?q=acoustic+guitar&as=on&as-show=on&otracker=AS_Query_HistoryAutoSuggest_1_9_na_na_na&otracker1=AS_Query_HistoryAutoSuggest_1_9_na_na_na&as-pos=1&as-type=HISTORY&suggestionId=acoustic+guitar&requestId=979dca32-ba12-41f5-8d46-7514f22ada35&as-backfill=on&sort=relevance&page="
+        + str(i)
+    )
+    res = scraper.get(url)
+    soup = BeautifulSoup(res.text, "lxml")
 
-for i in name:
-    nameList.append(i.text)
+    box = soup.find("div", class_="DOjaWF YJG4Cf")  # Container for the products
 
-price = soup.find_all("div", class_="Nx9bqj")
-for p in price:
-    priceList.append(p.text)
+    if box:
+        # Names
+        names = box.find_all("a", class_="wjcEIp")
+        page_name_list = [name.text for name in names]
 
+        # Prices
+        prices = box.find_all("div", class_="Nx9bqj")
+        page_price_list = [price.text for price in prices]
 
-review = soup.find_all("div", class_="XQDdHH")
-for r in review:
-    reviewList.append(r.text)
+        # Reviews
+        reviews = box.find_all("div", class_="XQDdHH")
+        page_review_list = [
+            reviews[i].text if i < len(reviews) else "0"
+            for i in range(len(page_name_list))
+        ]
 
+        # Total Reviews
+        total_reviews = box.find_all("span", class_="Wphh3N")
+        page_total_rev_list = [
+            total_reviews[i].text if i < len(total_reviews) else "0"
+            for i in range(len(page_name_list))
+        ]
 
-totalRev = soup.find_all("span", class_="Wphh3N")
-for t in totalRev:
-    TotalRev.append(t.text)
+        # Append current page data to the main lists
+        nameList.extend(page_name_list)
+        priceList.extend(page_price_list)
+        reviewList.extend(page_review_list)
+        TotalRev.extend(page_total_rev_list)
 
-print(TotalRev)
+        # Print status after each page
+        print(
+            f"Page {i} - Names: {len(nameList)}, Prices: {len(priceList)}, Reviews: {len(reviewList)}, Total Reviews: {len(TotalRev)}"
+        )
 
-""" df = pd.DataFrame({"priceList": priceList})
-print(df) """
+# Create a DataFrame
+df = pd.DataFrame(
+    {
+        "Name": nameList,
+        "Price": priceList,
+        "Review": reviewList,
+        "Total Review": TotalRev,
+    }
+)
+
+# Save the DataFrame to a CSV file
+df.to_csv("18-Flipkart_products.csv", index=False)
