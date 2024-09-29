111307381a5fc8ee5e4f4ccd48d7295d1f184925
rubaiiyat <abirr1516@gmail.com>
2024-09-29 23:34:11 +0600

    Scrap Full Page

--- 02-Beautifulsoup-And-Request-Exercise/01-Scrap-quotes.py
+++ 02-Beautifulsoup-And-Request-Exercise/01-Scrap-quotes.py
@@ -9,19 +9,20 @@ url = requests.get("https://quotes.toscrape.com/page/1/")
 soup = BeautifulSoup(url.text, "html.parser")
 
 
-mainDiv = soup.find("div", class_="quote")
+mainDiv = soup.find_all("div", class_="quote")
+print(len(mainDiv))
+for div in mainDiv:
+    content = div.find("span", class_="text").text
+    quotes["content"].append(content)
 
-content = mainDiv.find("span", class_="text").text
-quotes["content"].append(content)
+    author = div.find("small", class_="author").text
+    quotes["author"].append(author)
 
-author = mainDiv.find("small", class_="author").text
-quotes["author"].append(author)
-
-tags = mainDiv.find_all("div", class_="tags")
-for t in tags:
-    all_tag = t.find_all("a", class_="tag")
-    for tag in all_tag:
-        quotes["category"].append(tag.text)
+    tags = div.find_all("div", class_="tags")
+    for t in tags:
+        all_tag = t.find_all("a", class_="tag")
+        for tag in all_tag:
+            quotes["category"].append(tag.text)
 
 
 print(quotes)
